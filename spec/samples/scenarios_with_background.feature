b99693d3253be159fa43fbf851934c5a95f48501
Pablo Lacerda de Miranda <pablolm@yahoo-inc.com>
2013-09-19 16:16:52 -0700

    adds 2 scenarios with background test

---
+++ spec/samples/scenarios_with_background.feature
@@ -0,0 +1,21 @@
+Feature: Y! Mail ViewMastr App
+    In order see all my attachments  As a Y! Mail user
+    I want an App to interact with my attachments
+
+Background:
+    Given I am logged in MintyFresh with: User1
+    And I make sure the Applications list is expanded
+    And I wait for 3 seconds
+
+@smoke
+Scenario: Clicking email subject in Attachments should open email in new tab
+    And I click on "Attachments" from Applications View
+    And I click on the email subject: "bunch of attachments" (Attachments)
+    Then I should see a new tab labeled either as "bunch of attachments" or "Message"
+
+@smoke
+Scenario: Dummy Scenario
+    And I click on "Attachments" from Applications View
+    And I click on the email subject: "bunch of attachments" (Attachments)
+    Then I should see a new tab labeled either as "bunch of attachments" or "Message"
+    And I finish the test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,21 @@ describe FeatureSplitter do
         expect(scenarios[0].mount.strip).to eq(content)
     end
 
+    it "parsing a feature with background and more than 1 scenario, all the scenarios should have background block" do
+        content = File.read(@sample_dir + '/scenarios_with_background.feature')
+
+        scenarios = @splitter.extract_scenarios(content)
+
+        expect(scenarios.length).to be(2)
+
+        expect(scenarios[0].name).to include "Clicking email subject in Attachments should open email in new tab"
+        expect(scenarios[0].background).to include "Given I am logged in MintyFresh with: User1"
+        expect(scenarios[0].background).to include "And I wait for 3 seconds"
+
+        expect(scenarios[1].name).to include "Dummy Scenario"
+        expect(scenarios[1].background).to include "Given I am logged in MintyFresh with: User1"
+        expect(scenarios[1].background).to include "And I wait for 3 seconds"
+        expect(scenarios[1].body).to include "And I finish the test"
+    end
+
 end
